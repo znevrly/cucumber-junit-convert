25a05e0da4387b4b87681d9c1d8ba32256b4c1b1
Dikshita25 <Dikshitashirodkar25@gmail.com>
2021-07-24 13:02:45 +0530

    add handling in the converter to include attachments

--- index.js
+++ index.js
@@ -1,67 +1,91 @@
 const builder = require('junit-report-builder');
-const fs = require("fs");
+const fs = require('fs');
 const jsonFile = require('jsonfile');
 
 function convert(options) {
-    const jsonFileResult = jsonFile.readFileSync(options.inputJsonFile);
-    jsonFileResult.forEach(function (feature) {
-        let durationInSec = 0;
-        const suite = builder.testSuite().name(feature.name);
-        feature.elements.forEach(function (scenario) {
-            if (scenario.type == "background") {
-                return;
-            }
+  const jsonFileResult = jsonFile.readFileSync(options.inputJsonFile);
+  jsonFileResult.forEach(function(feature) {
+    let durationInSec = 0;
+    const suite = builder.testSuite().name(feature.name);
+    feature.elements.forEach(function(scenario) {
+      if (scenario.type == 'background') {
+        return;
+      }
 
-            const result = getScenarioSummary(scenario);
-            durationInSec += result.duration;
-            if (result.status === 'failed') {
-                suite.testCase()
-                     .name(scenario.name)
-                     .className(scenario.id)
-                     .failure(result.message)
-                     .time(result.duration);
-            }
-            else if (result.status === 'skipped') {
-                suite.testCase()
-                     .name(scenario.name)
-                     .className(scenario.id)
-                     .skipped()
-                     .time(result.duration);
-            }
-            else {
-                suite.testCase()
-                     .name(scenario.name)
-                     .className(scenario.id)
-                     .time(result.duration);
-            }
-        });
-        suite.time(durationInSec);
+      const result = getScenarioSummary(scenario);
+      durationInSec += result.duration;
+      if (result.status === 'failed') {
+        if (result.embeddings.length) {
+          suite
+            .testCase()
+            .name(scenario.name)
+            .className(scenario.id)
+            .standardError(result.message)
+            .errorAttachment(result.embeddings[0])
+            .time(result.duration);
+        } else {
+          suite
+            .testCase()
+            .name(scenario.name)
+            .className(scenario.id)
+            .failure(result.message)
+            .time(result.duration);
+        }
+      } else if (result.status === 'skipped') {
+        suite
+          .testCase()
+          .name(scenario.name)
+          .className(scenario.id)
+          .skipped()
+          .time(result.duration);
+      } else {
+        suite
+          .testCase()
+          .name(scenario.name)
+          .className(scenario.id)
+          .time(result.duration);
+      }
     });
-    builder.writeTo(options.outputXmlFile);  
+    suite.time(durationInSec);
+  });
+  builder.writeTo(options.outputXmlFile);
 }
 
 function getScenarioSummary(scenario) {
-    let status = 'passed';
-    let message = null;
-    let duration = 0;
-    scenario.steps.forEach(function (step) {
-        if (step.result.duration) {
-            duration += step.result.duration; 
-        }
-        if (step.result.status == 'failed') {
-            status = 'failed';
-            message = step.result.error_message;
-        } else if (status == 'passed' && (step.result.status == 'pending' || step.result.status == 'skipped')) {
-            status = 'skipped';
-        }
-    });
-    const durationInSec = duration / 1000000000; //ns to sec
-    return { status: status, 
-             message: message,
-             duration: durationInSec
-        }; 
+  let status = 'passed';
+  let message = null;
+  let duration = 0;
+  let embeddings = [];
+
+  scenario.steps.forEach(function(step) {
+    if (step.result.duration) {
+      duration += step.result.duration;
+    }
+
+    if (step.embeddings) {
+      embeddings.push(step.embeddings[0].data);
+    }
+
+    if (step.result.status == 'failed') {
+      status = 'failed';
+      message = step.result.error_message;
+    } else if (
+      status == 'passed' &&
+      (step.result.status == 'pending' || step.result.status == 'skipped')
+    ) {
+      status = 'skipped';
+    }
+  });
+  const durationInSec = duration / 1000000000; //ns to sec
+
+  return {
+    status: status,
+    message: message,
+    duration: durationInSec,
+    embeddings
+  };
 }
 
 module.exports = {
-    convert: convert
-};  
+  convert: convert
+};
