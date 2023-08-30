f2fa1533c11dccec127e34b5d63df46e3c7562f7
Onyinye Aladiume <onyinye_aladiume@discovery.com>
2023-08-30 13:50:23 -0700

    test

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function convert(options) {
             .errorAttachment(result.embeddings[0] + result.embeddings[2]) 
             .failure(result.message)
             .time(result.duration);
-          console.log(result.embeddings[2])
+          console.log('Result: ', result.embeddings[2])
         } else {
           suite
             .testCase()
