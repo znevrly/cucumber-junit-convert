96eb27a952b477b55a3dfb897fe049d2c607027c
Onyinye Aladiume <onyinye_aladiume@discovery.com>
2023-08-31 10:33:04 -0700

    test

--- index.js
+++ index.js
@@ -71,8 +71,8 @@ function getScenarioSummary(scenario, options) {
       //  embeddings.push(step.embeddings[0].data);
       //  embeddings.push("\n");
       //  embeddings.push(step.embeddings[2].data);
-      for(let i of embeddings.length){
-        if(i == 0 || i== 2){
+      for(let i = 0; i < embeddings.length; i++){
+        if(i === 0 || i === 2){
           embeddings.push(step.embeddings[i].data)
         }
       }
